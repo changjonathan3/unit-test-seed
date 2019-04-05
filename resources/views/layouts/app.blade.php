61926060b208a718d9585cffb9e1c180414e418b
chang <jkc26@njit.edu>
2019-04-04 22:44:01 -0400

    Task:Generate 50 Users and 50 Cars (Make, Model, Year)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,10 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('about') }}">
+                                        Profile
+
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
